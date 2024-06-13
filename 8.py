93dd260f9c7edcf9ad6b5353e1e1f20ab2487cef
its-bhavya <bhavyaa716@gmail.com>
2024-06-13 12:02:47 +0530

    Add 8.py

---
+++ 8.py
@@ -0,0 +1,4 @@
+# program that concatenates two strings and returns the result.
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+print("Concatenated String: ", str1+str2)
