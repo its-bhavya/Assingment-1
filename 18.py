77543dff3587128d1512f33eb520ec9aa705bbd7
its-bhavya <bhavyaa716@gmail.com>
2024-06-14 18:31:19 +0530

    Updated 18.py

--- 18.py
+++ 18.py
@@ -1,18 +1,17 @@
 #python program that checks if two strings are anagrams of each other.
 str1 = input("Enter first string: ")
 str2 = input("Enter second string: ")
-str1_chars = []
-str2_chars = []
-for i in str1:
-    str1_chars.append(i)
-for i in str2:
-    str2_chars.append(i)
+
+str1_chars = list(str1.lower())
+str2_chars = list(str2.lower())
+
 str1_chars.sort()
 str2_chars.sort()
+
 if str1_chars == str2_chars:
     print("They are anagrams")
 else:
-    print("they aren't anagrams.")
+    print("They aren't anagrams.")
 
     
 
